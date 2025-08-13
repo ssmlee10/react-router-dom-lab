b2e5deba51fac91aa759cb699d40db3de0b59391
Sharon Lee <ssmlee10@gmail.com>
2025-08-13 00:27:58 -0700

    create addBox function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import NavBar from "./components/NavBar/NavBar";
 import MailboxForm from './components/MailboxForm/MailboxForm';
 import MailboxList from "./components/MailboxList/MailboxList";
-// import MailboxDetails from './components/MailboxDetails/MailboxDetails';
+import MailboxDetails from './components/MailboxDetails/MailboxDetails';
 import { useState } from "react";
 import { Route, Routes } from "react-router";
 
@@ -12,8 +12,14 @@ const App = () => {
 
   // creates a new mailbox
   // it should accept form data for a new mailbox and setMailboxes state accordingly
-  const addBox = () => {
+  const addBox = (mailboxData) => {
+    // create a new mailbox
+    // copy objects in mailboxData
+    //_id: mailboxes.length+1 assigns new id to mailbox as mailboxes.length +1
+    const newMailbox = {...mailboxData, _id: mailboxes.length +1,}
 
+    // ... creates a new array with existing mailboxes + adds new ones at the end from newMailbox, pushes into the setMailboxes state
+    setMailboxes([...mailboxes, newMailbox]);
   }
 
   return (
@@ -22,8 +28,8 @@ const App = () => {
       <Routes>
         <Route path='/' element={<h1>Post Office</h1>}></Route>
         <Route path="/mailboxes" element={<MailboxList />} />
-        <Route path="/new-mailbox" element={<MailboxForm />} />
-        {/* <Route path="/mailboxes/:mailboxId" element={<MailboxDetails />} /> */}
+        <Route path="/new-mailbox" element={<MailboxForm addBox={addBox}/>} />
+        <Route path="/mailboxes/:mailboxId" element={<MailboxDetails />} />
       </Routes>
     </>
   );
