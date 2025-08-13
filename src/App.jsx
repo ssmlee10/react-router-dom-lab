ce1fedfb32505d4e742504ee62efee1537bfb0d6
Sharon Lee <ssmlee10@gmail.com>
2025-08-12 21:28:00 -0700

    import route and routes from react-router, create route for each destination

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import NavBar from './components/NavBar/NavBar';
+import NavBar from "./components/NavBar/NavBar";
 // import MailboxForm from './components/MailboxForm/MailboxForm';
-// import MailboxList from './components/MailboxList/MailboxList';
+import MailboxList from "./components/MailboxList/MailboxList";
 // import MailboxDetails from './components/MailboxDetails/MailboxDetails';
-
+import { useState } from "react";
+import { Route, React } from "react-router";
 
 const App = () => {
-  
   return (
     <>
-    <NavBar />
-    <h1>Hello World!</h1>
-    {/* <MailboxForm />
-    <MailboxList />
-    <MailboxDetails /> */}
+      <NavBar />
+      <Routes>
+        <Route path='/'><h1>Mailbox List</h1></Route>
+        <Route path="/mailboxes" element={<MailboxList mail={mail} />} />
+        <Route path="/new-mailbox" element={<MailboxForm />} />
+        <Route path="/mailboxes/:mailboxId" element={<MailboxDetails />} />
+      </Routes>
     </>
-  )
+  );
 };
 
 export default App;
