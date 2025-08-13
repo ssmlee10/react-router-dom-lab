07890de0a4503354f0c650877014cf5934cf1134
Sharon Lee <ssmlee10@gmail.com>
2025-08-12 17:56:36 -0700

    create components files, setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import NavBar from './components/NavBar/NavBar';
+import MailboxForm from './components/MailboxForm/MailboxForm';
+import MailboxList from './components/MailboxList/MailboxList';
+import MailboxDetails from './components/MailboxDetails/MailboxDetails';
+
+
 const App = () => {
   
   return <h1>Hello world!</h1>;
