8712d1481c85700d0b5628b7a59fee5bf474cc00
Sharon Lee <ssmlee10@gmail.com>
2025-08-12 18:17:45 -0700

    update navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,21 @@
+import { Link } from "react-router";
+
 const NavBar = () => {
-    return (
-        <nav>
-            <ul>
-                <li>Home</li>
-                <li>Mailboxes</li>
-                <li>New Mailbox</li>
-            </ul>
-        </nav>
-    )
-}
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/mailboxes'>Mailboxes</Link>
+        </li>
+        <li>
+          <Link to='/new-mailbox'>New Mailbox</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default NavBar;
