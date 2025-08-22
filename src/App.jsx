f7c4a3525e0c9579be7884b6b04301e56a4e85c5
Sharon Lee <ssmlee10@gmail.com>
2025-08-21 20:31:29 -0700

    fix mailbox form to display

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,25 @@
 import NavBar from "./components/NavBar/NavBar";
 import MailboxForm from './components/MailboxForm/MailboxForm';
 import MailboxList from "./components/MailboxList/MailboxList";
-import MailboxDetails from './components/MailboxDetails/MailboxDetails';
+import MailboxDetails from "./components/MailboxDetails/MailboxDetails";
 import { useState } from "react";
 import { Route, Routes } from "react-router";
-
+import React from 'react';
 
 const App = () => {
-
-  const [mailboxes, setMailboxes ] = useState([]);
-
-  // creates a new mailbox
-  // it should accept form data for a new mailbox and setMailboxes state accordingly
+  const [mailboxes, setMailboxes] = useState([]);
   const addBox = (mailboxData) => {
-    // create a new mailbox
-    // copy objects in mailboxData
-    //_id: mailboxes.length+1 assigns new id to mailbox as mailboxes.length +1
-    const newMailbox = {...mailboxData, _id: mailboxes.length +1,}
-
-    // ... creates a new array with existing mailboxes + adds new ones at the end from newMailbox, pushes into the setMailboxes state
+    const newMailbox = { ...mailboxData, _id: mailboxes.length + 1 };
     setMailboxes([...mailboxes, newMailbox]);
-  }
+  };
 
   return (
     <>
       <NavBar />
       <Routes>
-        <Route path='/' element={<h1>Post Office</h1>}></Route>
+        <Route path="/" element={<h1>Post Office</h1>}></Route>
         <Route path="/mailboxes" element={<MailboxList />} />
-        <Route path="/new-mailbox" element={<MailboxForm addBox={addBox}/>} />
+        <Route path="/new-mailbox" element={<MailboxForm />} />
         <Route path="/mailboxes/:mailboxId" element={<MailboxDetails />} />
       </Routes>
     </>
--- src/components/MailboxForm/MailboxForm.jsx
+++ src/components/MailboxForm/MailboxForm.jsx
@@ -1,31 +1,54 @@
-const MailboxForm = () => {
-const boxSize = {
-    small: 'small',
-    medium: 'medium',
-    large: 'large',
-}
+import { useState } from "react";
+import { useNavigate } from "react-router";
 
-    return (
-        <main>
-            <h2>New Mailbox</h2>
-            <form>
-                {/* Boxholder Name */}
-                <label htmlFor="boxholder">Enter a Boxholder:</label>
-                <input type="text" id='boxholder' name='boxholder'
-                value='Boxholder name'/>
-                <label htmlFor="boxSize">Select a Box Size:</label>
-                <select>
-                    <option value="">-- Select --</option>
-                    {Object.entries(boxSize).map(([key, value]) => (
-                        <option key={key} value = {key}>
-                            {value}
-                        </option>
-                    ))}
-                </select>
-                <button>Submit</button>
-            </form>
-        </main>
-    )
-}
+const MailboxForm = ({ addBox }) => {
+  const [formData, setFormData] = useState({
+    boxOwner: "",
+    boxSize: "Small",
+  });
+
+  const boxSize = {
+    Small: "Small",
+    Medium: "Medium",
+    Large: "Large",
+  };
+
+  const handleChange = ({ target }) => {
+    setFormData({ ...formData, [target.boxholder]: target.value });
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    addBox(formData);
+    setFormData({ boxOwner: "", boxSize: "Small" });
+  };
+
+  return (
+    <main>
+      <h2>New Mailbox</h2>
+      <form onSubmit={handleSubmit}>
+        {/* Boxholder Name */}
+        <label htmlFor="boxholder">Enter a Boxholder:</label>
+        <input
+          type="text"
+          id="boxholder"
+          name="boxholder"
+          value={formData.boxOwner}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="boxSize">Select a Box Size:</label>
+        <select>
+          {Object.entries(boxSize).map(([key, value]) => (
+            <option key={key} value={key}>
+              {value}
+            </option>
+          ))}
+        </select>
+        <button type="submit">Create a Mailbox</button>
+      </form>
+    </main>
+  );
+};
 
 export default MailboxForm;
